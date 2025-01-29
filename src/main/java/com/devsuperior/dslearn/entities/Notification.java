8d67c0474bbf37622c6271ee0200a27ae8cdabc4
dupontcam <125046483+dupontcam@users.noreply.github.com>
2025-01-29 16:04:03 -0300

    Notification

--- src/main/java/com/devsuperior/dslearn/entities/Notification.java
+++ src/main/java/com/devsuperior/dslearn/entities/Notification.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dslearn.entities;
 
 import java.time.Instant;
+import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,100 +12,101 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_notification")
 public class Notification {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String text;
-  private Instant moment;
-  private Boolean read = false;
-  private String route;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String text;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
 
-  @ManyToOne
-  @JoinColumn(name = "user_id")
-  private User user;
+	@Column(name = "\"read\"") // Escapa o nome da coluna
+	private boolean read;
+	private String route;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public Notification() {
+	}
 
-  public Notification(){}
+	public Notification(Long id, String text, Instant moment, boolean read, String route, User user) {
+		super();
+		this.id = id;
+		this.text = text;
+		this.moment = moment;
+		this.read = read;
+		this.route = route;
+		this.user = user;
+	}
 
-  public Notification(Long id, String text, Instant moment, Boolean read, String route, User user) {
-    this.id = id;
-    this.text = text;
-    this.moment = moment;
-    this.read = read;
-    this.route = route;
-    this.user = user;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public String getText() {
+		return text;
+	}
 
-  public String getText() {
-    return text;
-  }
+	public void setText(String text) {
+		this.text = text;
+	}
 
-  public void setText(String text) {
-    this.text = text;
-  }
+	public Instant getMoment() {
+		return moment;
+	}
 
-  public Instant getMoment() {
-    return moment;
-  }
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
 
-  public void setMoment(Instant moment) {
-    this.moment = moment;
-  }
+	public boolean isRead() {
+		return read;
+	}
 
-  public Boolean getRead() {
-    return read;
-  }
+	public void setRead(boolean read) {
+		this.read = read;
+	}
 
-  public void setRead(Boolean read) {
-    this.read = read;
-  }
+	public String getRoute() {
+		return route;
+	}
 
-  public String getRoute() {
-    return route;
-  }
+	public void setRoute(String route) {
+		this.route = route;
+	}
 
-  public void setRoute(String route) {
-    this.route = route;
-  } 
+	public User getUser() {
+		return user;
+	}
 
-  public User getUser() {
-    return user;
-  }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    return result;
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Notification other = (Notification) obj;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    return true;
-  }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Notification other = (Notification) obj;
+		return Objects.equals(id, other.id);
+	}
 }
--- src/main/java/com/devsuperior/dslearn/entities/User.java
+++ src/main/java/com/devsuperior/dslearn/entities/User.java
@@ -1,13 +1,13 @@
 package com.devsuperior.dslearn.entities;
 
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,97 +25,83 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	@Column(unique = true)
 	private String email;
 	private String password;
-
-  @OneToMany(mappedBy = "user")
-  private List<Notification> notifications = new ArrayList<Notification>();
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
 		joinColumns = @JoinColumn(name = "user_id"),
 		inverseJoinColumns = @JoinColumn(name = "role_id"))	
 	private Set<Role> roles = new HashSet<>();
 	
+	@OneToMany(mappedBy = "user")
+	private List<Notification> notifications = new ArrayList<>();
+	
 	public User() {
 	}
 
-  public User(Long id, String name, String email, String password, Set<Role> roles) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.roles = roles;
-  }
+	public User(Long id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public String getEmail() {
+		return email;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-  public String getPassword() {
-    return password;
-  }
+	public String getPassword() {
+		return password;
+	}
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-  public Set<Role> getRoles() {
-    return roles;
-  }
+	public Set<Role> getRoles() {
+		return roles;
+	}
 
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
-  }
+	public List<Notification> getNotifications() {
+		return notifications;
+	}
 
-  public List<Notification> getNotifications() {
-    return notifications;
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    User other = (User) obj;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    return true;
-  }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
 }
